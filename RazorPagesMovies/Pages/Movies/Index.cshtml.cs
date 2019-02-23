e19aeb6b509306c0d056388c3ac956d526812518
EvanBS <evandiesp@gmail.com>
2019-02-23 21:21:41 +0200

    rating and data annotation

--- RazorPagesMovies/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovies/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovies.Models;
 
@@ -22,7 +23,27 @@ namespace RazorPagesMovies.Pages.Movies
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            /*
+            var movies = from m in _context.Movie
+                         select m;
+            */
+
+            var movies = _context.Movie.Where(m => m != null);
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            Movie = await movies.ToListAsync();
         }
+
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
     }
 }
